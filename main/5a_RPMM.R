51c7d34384290fb264ba113b1581a68e5e2eaa5f
ydavidchen <david.chen.gr@dartmouth.edu>
2024-08-12 20:43:56 -0700

    Global methylation analysis import

---
+++ main/5a_RPMM.R
@@ -0,0 +1,58 @@
+# Global Methylation Clusters w/ Recursively Partitioned Mixture Model
+
+rm(list=ls())
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+source("../utils.R")
+
+SELE_UNIV <- SELE_UNIV <- read.table(PATH_UNIV)[,1] #25081 CpGs
+
+getRPMMSampOrder <- function(rpmmClusters, Y_inv, sampleIdName) {
+  #'@description Retrieves sample orders fo heat map visualization
+  #'@param rpmmClusters data.frame with row.names = sample ID & 1 column named RPMM with cluster assignments
+  #'@param Y_inv Input matrix for RPMM computation
+  sampOrder <- c()
+  for(r in names(table(rpmmClusters$RPMM))) {
+    samps <- rownames(rpmmClusters)[rpmmClusters$RPMM == r]
+    clu <- t(Y_inv[rownames(Y_inv) %in% as.character(samps), ])
+    s_i <- seriation::seriate(clu, margin=2)
+    so_i <- seriation::get_order(s_i)
+    sampOrder <- c(sampOrder, samps[so_i])
+  }
+  sampOrder <- data.frame(
+    V1 = sampOrder,
+    RPMMSampleOrder = 1:length(sampOrder)
+  )
+  colnames(sampOrder)[1] <- sampleIdName
+  return(sampOrder)
+}
+
+run_custom_rpmm <- function(dnam, sele, maxlevel=2, sampleIdName="Sample_ID") {
+  require(RPMM)
+  Y_inv <- t(dnam[sele, ])
+  sup_rpmm <- blcTree(Y_inv, verbose=1, maxlevel=maxlevel)
+  print(sup_rpmm)
+  plot(sup_rpmm)
+  
+  sup_class <- blcTreeLeafClasses(sup_rpmm)
+  res_rpmm <- data.frame(table(RPMM=sup_class, Sample_ID=rownames(Y_inv)))
+  res_rpmm <- subset(res_rpmm, Freq != 0)
+  res_rpmm$Freq <- NULL
+  
+  rownames(res_rpmm) <- res_rpmm$Sample_ID #for sample order helper
+  sampOrder <- getRPMMSampOrder(res_rpmm, Y_inv, sampleIdName)
+  res_rpmm <- merge(res_rpmm, sampOrder, by=sampleIdName)
+  return(res_rpmm)
+}
+
+main <- function() {
+  load_cohort1()
+  load_cohort2()
+  
+  c1_rpmm <- run_custom_rpmm(cohort1_betas, SELE_UNIV, "Sample_ID")
+  c2_rpmm <- run_custom_rpmm(cohort2_betas, SELE_UNIV, "Tube.number")
+
+  write.csv(c1_rpmm, paste0(DIR_RPMM,"Cohort1_RPMM.csv"), row.names=FALSE, quote=FALSE)
+  write.csv(c2_rpmm, paste0(DIR_RPMM,"Cohort2_RPMM.csv"), row.names=FALSE, quote=FALSE)
+}
+
+if(! interactive()) main()
---
+++ main/5b_global_dnam_heatmaps.R
@@ -0,0 +1,67 @@
+# Heatmaps for Global Methylation
+
+rm(list=ls())
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+library(pheatmap)
+source("../utils.R")
+source("../plotThemes.R")
+
+SELE_UNIV <- read.table(PATH_UNIV)[,1]
+
+CPG_ANNOT <- createCpGTrackingBars()
+
+ANNOT_COLORS[["BRCA1"]] <- c(Hyperme="black", Mutated="lightgray") #from plotThemes
+
+wrapper_hm <- function(mat, samp_annot, gaps_col=NULL) {
+  #'@description Heatmap w/ optional custom ordering
+  #'@param gaps_col Position to make gaps. If NULL, order ignored & hclust will be run
+  ph <- pheatmap(
+    mat,
+    show_rownames = FALSE,
+    cluster_cols = is.null(gaps_col),
+    treeheight_row = 0, #hide
+    border_color = NA,
+    fontsize = 12,
+    color = HEAT_COLS,
+    annotation_colors = ANNOT_COLORS,
+    annotation_row = CPG_ANNOT,
+    annotation_col = samp_annot,
+    gaps_col = gaps_col
+  )
+}
+
+# ---------------------- Load & Re-order/Match Cohort Data ---------------------- 
+load_cohort1()
+load_cohort2()
+
+rownames(cohort1_covars) <- cohort1_covars$Sample_ID
+rownames(cohort2_covars) <- cohort2_covars$Tube.number
+
+cohort1_covars <- cohort1_covars[order(cohort1_covars$RPMMSampleOrder), ]
+cohort2_covars <- cohort2_covars[order(cohort2_covars$RPMMSampleOrder), ]
+
+cohort1_betas <- cohort1_betas[SELE_UNIV, match(rownames(cohort1_covars), colnames(cohort1_betas))]
+cohort2_betas <- cohort2_betas[SELE_UNIV, match(rownames(cohort2_covars), colnames(cohort2_betas))]
+
+# ---------------------- Heat Maps w/ Unsupervised Clustering ----------------------
+## Global methylation w/ RPMM sample order across all samples (Fig.2): 
+wrapper_hm(cohort1_betas,  cohort1_covars[,c("HRD","RPMM")],  c(17,25))
+wrapper_hm(cohort2_betas,  cohort2_covars[,c("HRD","RPMM")],  c(35,48))
+
+
+## EWAS loci w/ hclust (Suppl. Fig):
+EWAS_CONCORD <- read.csv(PATH_DMPS[["both"]])
+
+wrapper_hm(cohort1_betas[EWAS_CONCORD$Name,], cohort1_covars[,c("HRD","AgeAtDx")])
+wrapper_hm(cohort2_betas[EWAS_CONCORD$Name,], cohort2_covars[,c("HRD","AgeAtDx")])
+
+
+## Global methylation for HRD BRCA1-altered w/ hclust (Reviewer Request):
+cohort1_sele <- subset(cohort1_covars, HRD=="Yes" & (BRCA.Status=="BRCA1" | BRCA1_Hypermeth=="Yes"))
+cohort2_sele <- subset(cohort2_covars, HRD=="Yes" & (BRCA.Status=="BRCA1" | isBRCA1Meth))
+
+cohort1_sele$BRCA1 <- ifelse(cohort1_sele$BRCA1_Hypermeth=="Yes", "Hyperme", "Mutated")
+cohort2_sele$BRCA1 <- ifelse(cohort2_sele$isBRCA1Meth, "Hyperme", "Mutated")
+
+wrapper_hm(cohort1_betas[,rownames(cohort1_sele)], cohort1_sele[,"BRCA1",drop=F])
+wrapper_hm(cohort2_betas[,rownames(cohort2_sele)], cohort2_sele[,"BRCA1",drop=F])
